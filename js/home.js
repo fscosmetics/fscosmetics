21aaf143b04e7b4a87e779b58070a7e85389f51e
rensramos <rensramos17@gmail.com>
2015-07-07 19:24:22 +0800

    add boilerplate

---
+++ js/home.js
@@ -0,0 +1,25 @@
+//var rd = rd || {};
+//  rd.init = function() {
+//      rd.addActiveClass();
+//  }
+//
+//rd.addActiveClass = function () {
+//  $(".navbar-nav>li").click(function(){
+//    $(".navbar-nav>li .active").removeClass("active");
+//    $(this).addClass("active");
+//  });
+//};
+//
+////$(document).ready(function () {
+////    $("ul.art-hmenu>li").on("click", "a", function (event) {
+////        debugger;
+////        $("#menu_wrapper .active").removeClass("active");
+////        $(this).addClass("active");
+////    });
+////});
+//
+//
+//
+//$(function(){
+//    rd.init();
+//});
