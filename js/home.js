187f4f024528afaddbab98f439a797809fdf9d8a
rensramos <rensramos17@gmail.com>
2015-07-09 18:27:21 +0800

    WIP navbar

--- js/home.js
+++ js/home.js
@@ -1,10 +1,17 @@
 var fs = fs || {};
 fs.init = function() {
     fs.initQuotesRotator();
+    fs.initChangeNavbar();
 }
 
 fs.initQuotesRotator = function(){
     $( '#cbp-qtrotator' ).cbpQTRotator();
+};
+
+fs.initChangeNavbar = function(){
+    $(window).scroll(function() {
+        $('#navbar-menu-container:in-viewport(300)').css('display','none');
+    });
 };
 
 $(function(){
