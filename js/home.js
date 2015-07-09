f228bb4d74193f347aa0a10b2dbc31a1e52382f2
rensramos <rensramos17@gmail.com>
2015-07-09 13:11:17 +0800

    add quotesRotator library

--- js/home.js
+++ js/home.js
@@ -1,25 +1,13 @@
-//var rd = rd || {};
-//  rd.init = function() {
-//      rd.addActiveClass();
-//  }
-//
-//rd.addActiveClass = function () {
-//  $(".navbar-nav>li").click(function(){
-//    $(".navbar-nav>li .active").removeClass("active");
-//    $(this).addClass("active");
-//  });
-//};
-//
-////$(document).ready(function () {
-////    $("ul.art-hmenu>li").on("click", "a", function (event) {
-////        debugger;
-////        $("#menu_wrapper .active").removeClass("active");
-////        $(this).addClass("active");
-////    });
-////});
-//
-//
-//
-//$(function(){
-//    rd.init();
-//});
+var fs = fs || {};
+fs.init = function() {
+    fs.initQuotesRotator();
+}
+
+fs.initQuotesRotator = function(){
+    $( '#cbp-qtrotator' ).cbpQTRotator();
+};
+
+$(function(){
+    fs.init();
+});
+
