f67c63842abe6854074ffdabeb1fffe3ba34ba81
rensramos <rensramos17@gmail.com>
2015-07-10 10:31:31 +0800

    add changeNavbar function

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 var fs = fs || {};
 fs.init = function() {
     fs.initQuotesRotator();
-    //fs.initChangeNavbar();
+    fs.initChangeNavbar();
     fs.initMenuOnHover();
 }
 fs.initMenuOnHover = function(){
@@ -21,33 +21,17 @@ fs.initQuotesRotator = function(){
 
 fs.initChangeNavbar = function(){
     $(window).scroll(function() {
-
-
+        var $navbarMenu = $('#navbar-menu-container');
+        var treshold = ($navbarMenu.innerHeight()-100);
+        if($(window).scrollTop() > treshold){
+            $navbarMenu.css('display','none');
+        }
+        else if($(window).scrollTop() < treshold){
+            $navbarMenu.css('display','block');
+        }
     });
-};
 
-//$(document).ready(function(){
-//    $(window).scroll(function(){
-//        var $brand = $('.navbar-brand');
-//        var $logo = $brand.find('.nav-logo');
-//        var treshold = ($('#cover').outerHeight()-34);
-//        if(window.innerWidth >= 768){
-//            if ($(window).scrollTop() > treshold ) {
-//                if (!$brand.hasClass('nav-scrolled')){
-//
-//                    $brand.addClass('nav-scrolled');
-//                    $logo.attr('src', '/img/crux-logo-inverse.png');
-//                    $('.navbar').addClass('navbar-crux-default');
-//                }
-//            }
-//            else if ($(window).scrollTop() < treshold) {
-//                $logo.attr('src', '/img/crux-brand-inverse.png');
-//                $brand.removeClass('nav-scrolled');
-//                $('.navbar').removeClass('navbar-crux-default');
-//            }
-//        }
-//    });
-//});
+};
 $(function(){
     fs.init();
 });
