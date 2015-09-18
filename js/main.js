d0f763963e4655eb2ddae062af237ce87582dcbe
rensramos <rensramos17@gmail.com>
2015-09-18 16:51:27 +0800

    fix js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 var fs = fs || {};
 fs.fsMainFunction = function() {
     fs.stickyNavbar();
-    fs.changeNavbarColor();
+    fs.navbarScroll();
 };
 
-fs.changeNavbarColor = function(){
-    var navbar = $('.navbar-default');
-    if(navbar == $('#services-section').offset.top-100){
-        navbar.addClass('test');
-    }
-}
+fs.navbarScroll = function(){
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50 && $(document).width() >= 992) {
+            $('nav').addClass('navbar-shrink');
+        } else {
+            $('nav').removeClass('navbar-shrink');
+        }
+    });
+};
 
 fs.scrollReveal = function(){
     window.sr = new scrollReveal();
