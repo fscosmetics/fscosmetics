6834899877b0c05b5cabeb3e850d880080ad7c91
rensramos <rensramos17@gmail.com>
2015-09-30 19:25:48 +0800

    add scroll reveal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var fs = fs || {};
 fs.fsMainFunction = function() {
+    fs.scrollRevealItems();
     fs.stickyNavbar();
     fs.iconAnimation();
 };
 
 
-
+fs.scrollRevealItems = function(){
+    window.sr = new scrollReveal();
+}
 
 fs.iconAnimation = function() {
     $('#icon-transition').click(function () {
