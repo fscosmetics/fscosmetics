0fcee10630a84473c0d53a9896a2d5a240b1256d
rensramos <rensramos17@gmail.com>
2015-10-02 19:44:09 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 var fs = fs || {};
 fs.fsMainFunction = function() {
     //fs.scrollRevealItems();
-    //fs.jpushNavbar();
+    fs.typeformOnLoad();
     fs.stickyNavbar();
     fs.iconAnimation();
 };
 
-
-fs.jpushNavbar = function(){
-    $('.toggle-menu-push').jPushMenu({closeOnClickLink: false});
-    $('.dropdown-toggle').dropdown();
+fs.typeformOnLoad = function(){
+    (function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()
 }
 
 fs.scrollRevealItems = function(){
