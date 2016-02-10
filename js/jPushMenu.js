a52badd8e54b92565d923b9e9300b06b4066d3d5
rensramos <rensramos17@gmail.com>
2016-02-10 18:26:05 +0800

    Fix contact typeform

--- js/jPushMenu.js
+++ js/jPushMenu.js
@@ -10,16 +10,16 @@
 		
 	$.fn.jPushMenu = function(customOptions) {
 		var o = $.extend({}, $.fn.jPushMenu.defaultOptions, customOptions);
-		
+
 		/* add class to the body.*/
-		
+
 		$('body').addClass(o.bodyClass);
 		$(this).addClass('jPushMenuBtn');
 		$(this).click(function() {
 			var target         = '',
 			push_direction     = '';
-			
-		
+
+
 			if($(this).is('.'+o.showLeftClass)) {
 				target         = '.cbp-spmenu-left';
 				push_direction = 'toright';
@@ -34,18 +34,18 @@
 			else if($(this).is('.'+o.showBottomClass)) {
 				target         = '.cbp-spmenu-bottom';
 			}
-			
+
 
 			$(this).toggleClass(o.activeClass);
 			$(target).toggleClass(o.menuOpenClass);
-			
+
 			if($(this).is('.'+o.pushBodyClass)) {
 				$('body').toggleClass( 'cbp-spmenu-push-'+push_direction );
 			}
-			
+
 			/* disable all other button*/
 			$('.jPushMenuBtn').not($(this)).toggleClass('disabled');
-			
+
 			return false;
 		});
 		var jPushMenu = {
--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 var fs = fs || {};
-fs.fsMainFunction = function() {
+fs.fsMainFunction = function($) {
     //fs.scrollRevealItems();
-    fs.stickyNavbar();
-    fs.iconAnimation();
+    fs.stickyNavbar($);
+    fs.iconAnimation($);
+    fs.typeFormShow();
+    $('.toggle-menu').jPushMenu({closeOnClickLink: false});
+    $('.dropdown-toggle').dropdown();
+
+    $(".contact-typeform").click(function(e){
+        $(".typeform-share").click();
+    });
+};
+
+fs.typeFormShow = function(){
+    (function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()
 };
 
 fs.scrollRevealItems = function(){
     window.sr = new scrollReveal();
 }
 
-fs.iconAnimation = function() {
+fs.iconAnimation = function($) {
     $('#icon-transition').on('click', function () {
         $(this).toggleClass('open');
     });
 }
 
-fs.stickyNavbar = function(){
+fs.stickyNavbar = function($){
     $(".category-nav-container").sticky({topSpacing:100});
 }
 
 $(function(){
-    fs.fsMainFunction();
+    fs.fsMainFunction($);
 });
