c27fae121f11bca59ec64cceda2361c94f7e9ab4
rensramos <rensramos17@gmail.com>
2016-02-15 19:43:23 +0800

    adjust animation-reveal speed

--- js/app.js
+++ js/app.js
@@ -62,13 +62,13 @@ fsCosmeticsApp.animation('.reveal-animation', function() {
     return {
         enter: function(element, done) {
             element.css('display', 'none');
-            element.fadeIn(500, done);
+            element.fadeIn(100, done);
             return function() {
                 element.stop();
             }
         },
         leave: function(element, done) {
-            element.fadeOut(500, done);
+            element.fadeOut(100, done);
             return function() {
                 element.stop();
             }
